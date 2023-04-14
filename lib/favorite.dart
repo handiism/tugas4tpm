3b0b2ddc55be5bb37d27db5f67b04ece78a81ded
handiism <mail.handira@gmail.com>
2023-04-14 07:16:15 +0700

    fixxxxxxxes

--- lib/favorite.dart
+++ lib/favorite.dart
@@ -45,8 +45,12 @@ class _FavoriteState extends State<Favorite> {
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Situs Favorite",
+        backgroundColor: Colors.grey[200],
+        title: const Center(
+          child: Text(
+            "Situs Favorite",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
       body: FutureBuilder(
